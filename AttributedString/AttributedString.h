ed6d3d2725bb6606e3eff36fc3f64d53b8a8aef4
Indragie Karunaratne <i@indragie.com>
2019-02-18 00:42:02 -0800

    Initial Commit

---
+++ AttributedString/AttributedString.h
@@ -0,0 +1,19 @@
+//
+//  AttributedString.h
+//  AttributedString
+//
+//  Created by Indragie Karunaratne on 2/18/19.
+//  Copyright © 2019 Indragie Karunaratne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AttributedString.
+FOUNDATION_EXPORT double AttributedStringVersionNumber;
+
+//! Project version string for AttributedString.
+FOUNDATION_EXPORT const unsigned char AttributedStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedString/PublicHeader.h>
+
+
---
+++ AttributedStringTests/AttributedStringTests.swift
@@ -0,0 +1,34 @@
+//
+//  AttributedStringTests.swift
+//  AttributedStringTests
+//
+//  Created by Indragie Karunaratne on 2/18/19.
+//  Copyright © 2019 Indragie Karunaratne. All rights reserved.
+//
+
+import XCTest
+@testable import AttributedString
+
+class AttributedStringTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
